72bf6a8eb70376a0612e61fb852414dd408b2fcc
Gian Millare <gianmillaregsm@gmail.com>
2021-06-02 11:15:43 -0700

    created templates and import flask bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 """Routes"""
 @app.route('/', methods=['GET'])
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
