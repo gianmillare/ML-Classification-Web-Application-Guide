f3d0f3a4c354fa50aedf6849974b3f2795602d6c
Gian Millare <gianmillaregsm@gmail.com>
2021-06-03 08:24:17 -0700

    edited index.html and included saving image files into the app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_bootstrap import Bootstrap
+from requests.api import request
+import os
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -7,6 +9,11 @@ Bootstrap(app)
 """Routes"""
 @app.route('/', methods=['GET'])
 def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            image_path = os.path.join('static', uploaded_file.filename)
+            uploaded_file.save(image_path)
     return render_template('index.html')
 
 if __name__ == "__main__":
